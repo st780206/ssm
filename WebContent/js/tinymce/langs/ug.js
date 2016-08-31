ba82e9244a559f6c0f96ee905a3350237b1223c8
tsong <tsong@tsong-PC>
2016-08-31 17:34:00 +0800

    tinymce 测试

---
+++ WebContent/js/tinymce/langs/ug.js
@@ -0,0 +1,45 @@
+tinymce.addI18n('ug',# Generated pot
+# Translators:
+# uyson <uyson@qq.com>, 2013
+# ياسىن ئېلى <yasinjaneli@gmail.com>, 2016
+msgid ""
+msgstr ""
+"Project-Id-Version: TinyMCE\n"
+"Report-Msgid-Bugs-To: http://moxiecode.com\n"
+"POT-Creation-Date: 2013-03-21 16:18:52+00:00\n"
+"PO-Revision-Date: 2016-08-24 10:49+0000\n"
+"Last-Translator: Joakim Lindkvist <info@moxiecode.com>\n"
+"Language-Team: Uighur (http://www.transifex.com/moxiecode/tinymce/language/ug/)\n"
+"MIME-Version: 1.0\n"
+"Content-Type: text/plain; charset=UTF-8\n"
+"Content-Transfer-Encoding: 8bit\n"
+"\"#: i18n:\n"
+"Language: ug\n"
+"Plural-Forms: nplurals=1; plural=0;\n"
+
+msgid "File"
+msgstr "ھۆججەت"
+
+msgid "Edit"
+msgstr "تەھرىرلەش"
+
+msgid "Insert"
+msgstr "قىستۇرۇش"
+
+msgid "View"
+msgstr "كۆرۈش"
+
+msgid "Format"
+msgstr "فورمات"
+
+msgid "Table"
+msgstr "جەدۋەل"
+
+msgid "Tools"
+msgstr "قۇرال"
+
+msgid ""
+"Rich Text Area. Press ALT-F9 for menu. Press ALT-F10 for toolbar. Press "
+"ALT-0 for help"
+msgstr "مول مەزمۇنلۇق تېكېسىت رامكىسى رايونىدا تىزىملىك ئۈچۈن ALT-F9 نى، قورال بالدىقى ئۈچۈن ALT-F10 نى، ياردەم ئۈچۈن ALT-0 نى بېسىڭ"
+);
---
+++ WebContent/js/tinymce/langs/vi_VN.js
@@ -0,0 +1,44 @@
+tinymce.addI18n('vi_VN',# Generated pot
+# Translators:
+# Ly Qui Chung <wolftg@gmail.com>, 2013
+msgid ""
+msgstr ""
+"Project-Id-Version: TinyMCE\n"
+"Report-Msgid-Bugs-To: http://moxiecode.com\n"
+"POT-Creation-Date: 2013-03-21 16:18:52+00:00\n"
+"PO-Revision-Date: 2016-08-24 10:47+0000\n"
+"Last-Translator: Joakim Lindkvist <info@moxiecode.com>\n"
+"Language-Team: Vietnamese (Viet Nam) (http://www.transifex.com/moxiecode/tinymce/language/vi_VN/)\n"
+"MIME-Version: 1.0\n"
+"Content-Type: text/plain; charset=UTF-8\n"
+"Content-Transfer-Encoding: 8bit\n"
+"\"#: i18n:\n"
+"Language: vi_VN\n"
+"Plural-Forms: nplurals=1; plural=0;\n"
+
+msgid "File"
+msgstr "Tập tin"
+
+msgid "Edit"
+msgstr "Sửa"
+
+msgid "Insert"
+msgstr "Thêm"
+
+msgid "View"
+msgstr "Xem"
+
+msgid "Format"
+msgstr "Định dạng"
+
+msgid "Table"
+msgstr "Bảng"
+
+msgid "Tools"
+msgstr "Công cụ"
+
+msgid ""
+"Rich Text Area. Press ALT-F9 for menu. Press ALT-F10 for toolbar. Press "
+"ALT-0 for help"
+msgstr "Khu vực soạn thảo. Nhấn ALT-F9 để hiện menu, ALT-F10 để hiện thanh công cụ. Cần trợ giúp nhấn ALT-0"
+);
